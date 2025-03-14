851f08c27f0623fea81fcdfea779d58b2ae94ca6
Justin Alcendor <kareem4209@gmail.com>
2025-03-14 18:17:08 -0500

    imported email.js, need to fix submit button from Contact form

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { RevealOnScroll } from "./RevealOnScroll";
+
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
@@ -24,14 +24,13 @@ export const LoadingScreen = ({ onComplete }) => {
   }, [onComplete]);
 
   return (
-    
-    <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-      <div className="mb-4 text-4xl font-mono font-bold">
+    <div className="fixed inset-0 z-100 bg-black text-indigo-100 flex flex-col items-center justify-center">
+      <div className="mb-4 text-5xl font-tahoma font-bold text-indigo">
         {text} <span className="animate-blink ml-1"> | </span>
       </div>
 
-      <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
-        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
+      <div className="w-[200px] h-[2px] bg-indigo-800 rounded relative overflow-hidden">
+        <div className="w-[40%] h-full bg-indigo-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
       </div>
     </div>
    
