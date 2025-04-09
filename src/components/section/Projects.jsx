445d1a53d4166978bd3b63929201165b134549e2
Justin Alcendor <jcabig24@outlook.com>
2025-04-09 18:18:40 -0500

    Finished Project component with links

--- src/components/section/Projects.jsx
+++ src/components/section/Projects.jsx
@@ -143,67 +143,6 @@ export const Projects = () => {
                   </span>
                 ))}
               </div>
-
-              <div className="flex justify-between items-center">
-                <a
-                  href="https://github.com/Kareem42/Restaurant-Orders-Analysis"
-                  className="text-indigo-400 font-mono hover:underline transition-colors"
-                >
-                  View Project ⇛{" "}
-                </a>
-              </div>
-            </div>
-
-            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
-              <h3 className="text-xl font-bold mb-2 font-mono"> Project 5</h3>
-              <p className="font-mono">Put description of the project here</p>
-
-              <div className="flex flex-wrap gap-2 mb-4 font-mono">
-                {["React", "Node.js", "AWS"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 font-mono text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                            hover:shadow-[0_2px_8px_rgba[(59,130,246,0.1)] transition"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-
-              <div className="flex justify-between items-center">
-                <a
-                  href="#"
-                  className="text-indigo-400 hover:underline font-mono transition-colors"
-                >
-                  View Project ⇛{" "}
-                </a>
-              </div>
-            </div>
-
-            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
-              <h3 className="text-xl font-bold mb-2 font-mono"> Project 6</h3>
-              <p className="font-mono">Put description of the project here</p>
-
-              <div className="flex flex-wrap gap-2 mb-4 font-mono">
-                {["React", "Node.js", "AWS"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 font-mono text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                            hover:shadow-[0_2px_8px_rgba[(59,130,246,0.1)] transition"
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-
-              <div className="flex justify-between items-center">
-                <a
-                  href="#"
-                  className="text-indigo-400 font-mono hover:underline transition-colors"
-                >
-                  View Project ⇛{" "}
-                </a>
-              </div>
             </div>
           </div>
         </div>
