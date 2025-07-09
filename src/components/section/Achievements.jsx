03f5b8ab8a784b2f364ee19e5e32572631110f3e
Kareem42 <kareem4209@gmail.com>
2025-07-08 19:12:34 -0500

    Added a achievement to the Achievement section

--- src/components/section/Achievements.jsx
+++ src/components/section/Achievements.jsx
@@ -114,6 +114,23 @@ export const Achievements = () => {
                                 </div>
                             </div>
                         </div>
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+                            <h3 className="text-xl font-bold mb-4 font-mono">Intermediate Web and Front-End Development</h3>
+                            <div className="flex flex-wrap gap-2">
+                                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                    Achievement on June 24, 2025
+                                </span>
+
+                                <div className="flex justify-between items-center">
+                                    <a
+                                        href="https://coursera.org/share/51afdb504cc1742e7a9ddf774883be15"
+                                        className="text-indigo-400 font-mono hover:underline transition-colors"
+                                    >
+                                        View Certificate ⇛{" "}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </RevealOnScroll>
