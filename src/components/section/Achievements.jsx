e66a2961f19300bf3e97d4322f7e4bbc6b206c68
Kareem42 <kareem4209@gmail.com>
2025-10-31 10:19:11 -0500

    Added new certification to achievement section

--- src/components/section/Achievements.jsx
+++ src/components/section/Achievements.jsx
@@ -1,139 +1,172 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 
 export const Achievements = () => {
-    return (
-        <section
-            id="acheivements"
-            className="min-h-screen flex items-center justify-center py-20"
-        >
-            <RevealOnScroll>
-                <div className="max-w-3xl mx-auto px-4">
-                    <h2 className="text-3xl font-mono font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-400 bg-clip-text text-transparent text-center">
-                        Certifications & Achievements
-                    </h2>
+  return (
+    <section
+      id="acheivements"
+      className="min-h-screen flex items-center justify-center py-20"
+    >
+      <RevealOnScroll>
+        <div className="max-w-3xl mx-auto px-4">
+          <h2 className="text-3xl font-mono font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-400 bg-clip-text text-transparent text-center">
+            Certifications & Achievements
+          </h2>
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
-                            <h3 className="text-xl font-bold mb-4 font-mono">The Business Intelligence Analyst Course 2024</h3>
-                            <div className="flex flex-wrap gap-2">
-                                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                                    Achievement on August 27, 2024
-                                </span>
-                                <div className="flex justify-between items-center">
-                                    <a
-                                        href="https://coursera.org/share/6e96cae70bf481144bec846c0445bd2b"
-                                        className="text-indigo-400 font-mono hover:underline transition-colors"
-                                    >
-                                        View Certificate ⇛{" "}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
-                            <h3 className="text-xl font-bold mb-4 font-mono">Introduction to Software Engineering</h3>
-                            <div className="flex flex-wrap gap-2">
-                                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                                    Achievement on May 29, 2025
-                                </span>
-
-                                <div className="flex justify-between items-center">
-                                    <a
-                                        href="https://coursera.org/share/0c3ce4f4cc907336fed8d70732c720ed"
-                                        className="text-indigo-400 font-mono hover:underline transition-colors"
-                                    >
-                                        View Certificate ⇛{" "}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
-                            <h3 className="text-xl font-bold mb-4 font-mono">Designing User Interfaces and Experiences - UI/UX</h3>
-                            <div className="flex flex-wrap gap-2">
-                                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                                    Achievement on June 2, 2025
-                                </span>
-
-                                <div className="flex justify-between items-center">
-                                    <a
-                                        href="https://coursera.org/share/c8127e95127a80179fd5186004e1dc11"
-                                        className="text-indigo-400 font-mono hover:underline transition-colors"
-                                    >
-                                        View Certificate ⇛{" "}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
-                            <h3 className="text-xl font-bold mb-4 font-mono">Introduction to HTML, CSS, & JavaScript</h3>
-                            <div className="flex flex-wrap gap-2">
-                                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                                    Achievement on June 6, 2025
-                                </span>
-
-                                <div className="flex justify-between items-center">
-                                    <a
-                                        href="https://coursera.org/share/e0c7b6388ae0623e3055f767ce02a94a"
-                                        className="text-indigo-400 font-mono hover:underline transition-colors"
-                                    >
-                                        View Certificate ⇛{" "}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
-                            <h3 className="text-xl font-bold mb-4 font-mono">Developing Websites and Front-Ends with Bootstrap</h3>
-                            <div className="flex flex-wrap gap-2">
-                                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                                    Achievement on June 10, 2025
-                                </span>
-
-                                <div className="flex justify-between items-center">
-                                    <a
-                                        href="https://coursera.org/share/38a609b1bbd1d146b7e6b44a4ba29510"
-                                        className="text-indigo-400 font-mono hover:underline transition-colors"
-                                    >
-                                        View Certificate ⇛{" "}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
-                            <h3 className="text-xl font-bold mb-4 font-mono">Delevoping Front-end Applications with React</h3>
-                            <div className="flex flex-wrap gap-2">
-                                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                                    Achievement on June 24, 2025
-                                </span>
-
-                                <div className="flex justify-between items-center">
-                                    <a
-                                        href="https://coursera.org/share/cdce4013c1f0a4559d14a91eabedd4db"
-                                        className="text-indigo-400 font-mono hover:underline transition-colors"
-                                    >
-                                        View Certificate ⇛{" "}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
-                            <h3 className="text-xl font-bold mb-4 font-mono">Intermediate Web and Front-End Development</h3>
-                            <div className="flex flex-wrap gap-2">
-                                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                                    Achievement on June 24, 2025
-                                </span>
-
-                                <div className="flex justify-between items-center">
-                                    <a
-                                        href="https://coursera.org/share/51afdb504cc1742e7a9ddf774883be15"
-                                        className="text-indigo-400 font-mono hover:underline transition-colors"
-                                    >
-                                        View Certificate ⇛{" "}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+              <h3 className="text-xl font-bold mb-4 font-mono">
+                The Business Intelligence Analyst Course 2024
+              </h3>
+              <div className="flex flex-wrap gap-2">
+                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                  Achievement on August 27, 2024
+                </span>
+                <div className="flex justify-between items-center">
+                  <a
+                    href="https://coursera.org/share/6e96cae70bf481144bec846c0445bd2b"
+                    className="text-indigo-400 font-mono hover:underline transition-colors"
+                  >
+                    View Certificate ⇛{" "}
+                  </a>
                 </div>
-            </RevealOnScroll>
-        </section>
-    );
-}
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+              <h3 className="text-xl font-bold mb-4 font-mono">
+                Introduction to Software Engineering
+              </h3>
+              <div className="flex flex-wrap gap-2">
+                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                  Achievement on May 29, 2025
+                </span>
+
+                <div className="flex justify-between items-center">
+                  <a
+                    href="https://coursera.org/share/0c3ce4f4cc907336fed8d70732c720ed"
+                    className="text-indigo-400 font-mono hover:underline transition-colors"
+                  >
+                    View Certificate ⇛{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+              <h3 className="text-xl font-bold mb-4 font-mono">
+                Designing User Interfaces and Experiences - UI/UX
+              </h3>
+              <div className="flex flex-wrap gap-2">
+                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                  Achievement on June 2, 2025
+                </span>
+
+                <div className="flex justify-between items-center">
+                  <a
+                    href="https://coursera.org/share/c8127e95127a80179fd5186004e1dc11"
+                    className="text-indigo-400 font-mono hover:underline transition-colors"
+                  >
+                    View Certificate ⇛{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+              <h3 className="text-xl font-bold mb-4 font-mono">
+                Introduction to HTML, CSS, & JavaScript
+              </h3>
+              <div className="flex flex-wrap gap-2">
+                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                  Achievement on June 6, 2025
+                </span>
+
+                <div className="flex justify-between items-center">
+                  <a
+                    href="https://coursera.org/share/e0c7b6388ae0623e3055f767ce02a94a"
+                    className="text-indigo-400 font-mono hover:underline transition-colors"
+                  >
+                    View Certificate ⇛{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+              <h3 className="text-xl font-bold mb-4 font-mono">
+                Developing Websites and Front-Ends with Bootstrap
+              </h3>
+              <div className="flex flex-wrap gap-2">
+                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                  Achievement on June 10, 2025
+                </span>
+
+                <div className="flex justify-between items-center">
+                  <a
+                    href="https://coursera.org/share/38a609b1bbd1d146b7e6b44a4ba29510"
+                    className="text-indigo-400 font-mono hover:underline transition-colors"
+                  >
+                    View Certificate ⇛{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+              <h3 className="text-xl font-bold mb-4 font-mono">
+                Delevoping Front-end Applications with React
+              </h3>
+              <div className="flex flex-wrap gap-2">
+                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                  Achievement on June 24, 2025
+                </span>
+
+                <div className="flex justify-between items-center">
+                  <a
+                    href="https://coursera.org/share/cdce4013c1f0a4559d14a91eabedd4db"
+                    className="text-indigo-400 font-mono hover:underline transition-colors"
+                  >
+                    View Certificate ⇛{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+              <h3 className="text-xl font-bold mb-4 font-mono">
+                Intermediate Web and Front-End Development
+              </h3>
+              <div className="flex flex-wrap gap-2">
+                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                  Achievement on June 24, 2025
+                </span>
+
+                <div className="flex justify-between items-center">
+                  <a
+                    href="https://coursera.org/share/51afdb504cc1742e7a9ddf774883be15"
+                    className="text-indigo-400 font-mono hover:underline transition-colors"
+                  >
+                    View Certificate ⇛{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shado-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+              <h3 className="text-xl font-bold mb-4 font-mono">
+                Spring Boot 3, Spring 6, Hibernate
+              </h3>
+              <div className="flex flex-wrap gap-2">
+                <span className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                  Achievement on December 24, 2025
+                </span>
+
+                <div className="flex justify-between items-center">
+                  <a
+                    href="https://coursera.org/share/51afdb504cc1742e7a9ddf774883be15"
+                    className="text-indigo-400 font-mono hover:underline transition-colors"
+                  >
+                    View Certificate ⇛{" "}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </RevealOnScroll>
+    </section>
+  );
+};
