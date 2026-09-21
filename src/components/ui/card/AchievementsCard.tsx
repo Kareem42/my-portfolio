8d3c69c25b070b7ab39dc02c638f15c8d9ee98b8
Kareem42 <kareem4209@gmail.com>
2026-09-21 15:47:45 -0500

    Achievement section reorg

---
+++ src/components/ui/card/AchievementsCard.tsx
@@ -0,0 +1,38 @@
+import type { Achievements } from "../../../types/index";
+
+type AchievementCardProps = Achievements;
+
+export const AchievementsCard = ({
+  title,
+  dateAchieved,
+  certLink,
+}: AchievementCardProps) => {
+  return (
+    <div className="flex flex-col p-6 rounded-xl border border-border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+      <h3 className="text-xl font-bold mb-2 font-mono">{title}</h3>
+
+      <div className="flex flex-wrap gap-2 mb-4 mt-4 font-mono">
+        {dateAchieved.map((date) => (
+          <span
+            key={date}
+            className="font-mono bg-blue-500/10 text-indigo-400 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                  hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition"
+          >
+            {date}
+          </span>
+        ))}
+      </div>
+
+      <div className="mt-auto flex justify-between items-center">
+        <a
+          href={certLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-indigo-400 font-mono hover:underline transition-colors"
+        >
+          View Certiifcate ⇛{" "}
+        </a>
+      </div>
+    </div>
+  );
+};
