f1d4834570105f43e5c81530716b286e310d82ec
Kareem42 <kareem4209@gmail.com>
2026-09-23 12:53:04 -0500

    toasted added again

--- src/components/section/Contact.tsx
+++ src/components/section/Contact.tsx
@@ -1,27 +1,35 @@
 import { useState } from "react";
 import { RevealOnScroll } from "../RevealOnScroll";
+import { toast, Toaster } from "../ui/toast/toast";
 import emailjs from "emailjs-com";
 
 export const Contact = () => {
-
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
   });
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: {
+    preventDefault: () => void;
+    target: string | HTMLFormElement;
+  }) => {
     e.preventDefault();
 
     emailjs
-      .sendForm(import.meta.env.VITE_SERVICE_ID,import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_PUBLIC_KEY
-     
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_PUBLIC_KEY,
       )
       .then(() => {
         alert("Message Sent!");
         setFormData({ name: "", email: "", message: "" });
       })
-      .catch(() => alert("Oh! Something doesn't seem right. Please try again... "));
+      .catch(() =>
+        alert("Oh! Something doesn't seem right. Please try again... "),
+      );
   };
 
   return (
@@ -82,12 +90,22 @@ export const Contact = () => {
             </div>
 
             <div className="flex items-center justify-center">
-            <button
-              type="submit"
-              className="inline-flex font-mono items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
-            >
-              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">Send Message</span>
-            </button>
+              <Toaster>
+                <button
+                  onClick={() =>
+                    toast.add({
+                      title: "Message Sent",
+                      description: "Thank you for your message!",
+                    })
+                  }
+                  type="submit"
+                  className="inline-flex font-mono items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+                >
+                  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                    Send Message
+                  </span>
+                </button>
+              </Toaster>
             </div>
           </form>
         </div>
